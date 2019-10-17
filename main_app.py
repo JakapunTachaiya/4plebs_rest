f418f4dbe132fb0dbffdfd828355e67d8a607760
jakapun tachaiya <jakapun.tachaiya@gmail.com>
2019-10-16 18:51:26 -0700

    updated commit

--- main_app.py
+++ main_app.py
@@ -105,11 +105,13 @@ def stringToDateTime(dateTxt):
 def crawl4plebs(subboard,startId,endID):
     currentTid = startId
 
-    logFile = "logfile.csv"
-    if os.path.exists(logFile):
-        log_df = readFile(logFile)
-    else:
-        log_df = pd.DataFrame()
+    # logFile = "logfile.csv"
+    # if os.path.exists(logFile):
+    #     log_df = readFile(logFile)
+    # else:
+    #     log_df = pd.DataFrame()
+
+    basePath = str(startId)+"_"+str(endID)
 
     while(currentTid<=endID):
         json_result = getThreadById(subboard,currentTid)
@@ -126,18 +128,18 @@ def crawl4plebs(subboard,startId,endID):
             dateTxt = json_result[keys]["op"]['fourchan_date']
             dt = stringToDateTime(dateTxt)
 
-            folPath =  createFolder(subboard,dt.year,dt.month)
+            folPath =  createFolder(basePath,subboard,dt.year,dt.month)
 
             outPath = os.path.join(folPath,str(threadId))
             with open(outPath, 'w') as f:
                 f.write(dumptext)
 
-            log_dict ={}
-            log_dict['tid'] = str(threadId)
-            log_dict['board'] = subboard
-            log_dict['thread_date'] = dt.date()
-            log_dict['crawl_date'] = datetime.now().date()
-            log_df = log_df.append(log_dict, ignore_index=True)
+            # log_dict ={}
+            # log_dict['tid'] = str(threadId)
+            # log_dict['board'] = subboard
+            # log_dict['thread_date'] = dt.date()
+            # log_dict['crawl_date'] = datetime.now().date()
+            # log_df = log_df.append(log_dict, ignore_index=True)
 
             print 'tid:{} -{}'.format(currentTid,dt.date())
 
@@ -147,16 +149,16 @@ def crawl4plebs(subboard,startId,endID):
 
         currentTid= currentTid+1
 
-    log_df = log_df[['tid','thread_date','board','crawl_date']]
+    # log_df = log_df[['tid','thread_date','board','crawl_date']]
 
-    saveToCSV(log_df,'logfile')
+    # saveToCSV(log_df,'logfile')
     return
 
 
 
 
-def createFolder(subboard,year,month):
-    basePath = "4plebs_data"
+def createFolder(basePath,subboard,year,month):
+    basePath = os.path.join(basePath,"4plebs_data")
     year = str(year)
     month = str(month)
     if not os.path.exists(os.path.join(basePath,subboard,year,month)):
@@ -171,11 +173,12 @@ def main(argv):
 
     #105302896 date start 2017-01-01
 
-    startId = 105502207
+    startId = 105595786
     endId   = 105600000
 
     startId = int(sys.argv[1])
     endId = int(sys.argv[2])
+
     print startId,endId
     crawl4plebs('pol',startId,endId)
 
